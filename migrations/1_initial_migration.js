01cae6e3c7e29e40478adb45b93a3c37c7b1f26f
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-03-25 21:35:00 +0530

    Update 1_initial_migration.js

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,19 @@
-const Migrations = artifacts.require("Migrations");
+// const Migrations = artifacts.require("Migrations");
 
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+// module.exports = function (deployer) {
+//   deployer.deploy(Migrations);
+// };
+
+// var TodoList = artifacts.require('../contracts/TodoList.sol');
+
+// module.exports = function(deployer) {
+//     deployer.deploy(TodoList);
+// }
+
+const Migrations = artifacts.require("Migrations");
+var TodoList = artifacts.require('../contracts/TodoList.sol');
+
+module.exports = function(deployer) {
+    deployer.deploy(Migrations);
+    deployer.deploy(TodoList);
 };
