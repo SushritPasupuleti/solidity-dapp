adc05f08be5879e789d21bb37d27a12a6110c799
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-03-25 21:35:03 +0530

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+var TodoList = artifacts.require('../contracts/TodoList.sol');
 
 module.exports = function(deployer) {
-    deployer.deploy(Migrations);
-};
+    deployer.deploy(TodoList);
+}
